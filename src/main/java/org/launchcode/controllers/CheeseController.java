e1191812ff6dfd0f3c32e430c891df4bee38a872
Chris Bay <chris@chrisbay.net>
2017-03-05 11:48:33 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -0,0 +1,43 @@
+package org.launchcode.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+
+    static ArrayList<String> cheeses = new ArrayList<>();
+
+    // Request path: /cheese
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "My Cheeses");
+
+        return "cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+        cheeses.add(cheeseName);
+        return "redirect:";
+    }
+
+}
