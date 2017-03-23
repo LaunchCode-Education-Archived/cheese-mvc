020d04a44df198e45a3d7594916b48c78317aa00
Andrew Bell <andrew@recursivechaos.com>
2017-03-22 19:14:40 -0500

    Adds ability to add and view user.

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,37 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Andrew Bell on 3/22/17.
+ */
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model){
+        model.addAttribute("title", "View User");
+        return "users/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute User user, String verify){
+        if(verify.equals(user.getPassword())){
+            model.addAttribute("title", "Add User");
+            model.addAttribute("newUser", user.getUsername());
+            return "users/index";
+        } else {
+            model.addAttribute("title", "View User");
+            model.addAttribute("error", "Passwords do not match");
+            model.addAttribute(user);
+            return "users/add";
+        }
+    }
+
+}
