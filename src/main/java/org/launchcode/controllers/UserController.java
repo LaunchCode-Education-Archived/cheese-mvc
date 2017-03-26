f54657e261d95c52550b226e8df1aa51aa675f57
Chris Bay <chris@chrisbay.net>
2017-03-25 23:34:40 -0500

    Finish studio

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,38 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        return "user/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, User user, String verify) {
+
+        if (user.getPassword() != null
+                && user.getPassword().equals(verify)) {
+            model.addAttribute("name", user.getUsername());
+            return "user/index";
+        }
+
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("error", "Passwords do not match");
+        return "user/add";
+
+    }
+
+
+}
---
+++ src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,43 @@
+package org.launchcode.models;
+
+/**
+ * Created by LaunchCode
+ */
+public class User {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
