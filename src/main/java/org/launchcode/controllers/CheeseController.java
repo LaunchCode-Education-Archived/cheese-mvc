56c1f19b36ec4895a3f919bc8bddb629bc77a342
Elizabeth Niebauer <egniebauer@gmail.com>
2017-05-02 11:28:24 -0500

    video-views-pt2-end

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -3,6 +3,10 @@ package org.launchcode.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 /**
  * Created by LaunchCode
@@ -12,11 +16,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "cheese")
 public class CheeseController {
 
+    static ArrayList<String> cheeses = new ArrayList<>();
+
     // Request path: cheese/
     @RequestMapping(value = "")
     public String index(Model model) {
 
+        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
     }
+
+    // Request path: cheese/add
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    // Request path: cheese/add
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+
+        cheeses.add(cheeseName);
+
+        // Redirect to cheese/
+        return "redirect:";
+    }
+
 }
